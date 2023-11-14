86850aa87838950ae76cb36bff1d73bab2fecd58
ThangNQT <toanthang1999hp@gmail.com>
2023-11-14 17:29:41 +0700

    feat(BE_FE): add authen with microservices, antd and styled components

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,12 +1,12 @@
 import { createBrowserRouter, RouteObject, useRoutes } from 'react-router-dom';
 import { USER_ROUTES } from './constants';
-import UserLayout from '../layouts/UserLayout';
-import React from 'react';
-import LoginPage from '../pages/user/auth/LoginPage';
-import SignUpPage from '../pages/user/auth/SignUpPage';
-import ForgetPasswordPage from '../pages/user/auth/ForgetPasswordPage';
-import ResetPasswordPage from '../pages/user/auth/ResetPasswordPage';
-import HomePage from '../pages/user/public/HomePage';
+import UserLayout from '@/layouts/UserLayout';
+import HomePage from '@/pages/user/public/HomePage';
+import LoginPage from '@/pages/user/auth/LoginPage';
+import SignUpPage from '@/pages/user/auth/SignUpPage';
+import ForgetPasswordPage from '@/pages/user/auth/ForgetPasswordPage';
+import ResetPasswordPage from '@/pages/user/auth/ResetPasswordPage';
+
 
 const router: any = createBrowserRouter([
     {
