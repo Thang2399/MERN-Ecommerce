98b15bd86dacbab7257786ff139d0eed672b0fc6
Thang Nguyen <thangnqt1999hp@outlook.com>
2024-06-20 20:01:51 +0700

    feat(BE-FE): implement login and sign up with Google

--- client/src/layouts/AuthenLayoutWrapper/index.tsx
+++ client/src/layouts/AuthenLayoutWrapper/index.tsx
@@ -1,12 +1,48 @@
 import Image from '@/components/base/Image';
-import React from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Outlet, useLocation, useNavigate, useSearchParams } from 'react-router-dom';
 import { USER_ROUTES } from '@/routes/constants';
+import { setCookie } from 'typescript-cookie';
+import { COMMON_CONSTANTS } from '@/constants';
+import { setShowToastMessage } from '@/store/common';
+import { useDispatch } from 'react-redux';
 
 export default function AuthenLayoutWrapper(): JSX.Element {
     const location = useLocation();
     const pathname = location.pathname;
     const imageUrl = pathname === USER_ROUTES.LOGIN || pathname === USER_ROUTES.FORGET_PASSWORD ? '/assets/images/login-image.jpeg' : '/assets/images/signup-image.jpeg';
+
+    const [ searchParams, setSearchParams ] = useSearchParams();
+    const accessToken = searchParams.get('accessToken');
+    const refreshToken = searchParams.get('refreshToken');
+    const errorCode = searchParams.get('errorCode');
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if (accessToken && refreshToken) {
+            setCookie(COMMON_CONSTANTS.ACCESS_TOKEN, accessToken);
+            localStorage.setItem(COMMON_CONSTANTS.REFRESH_TOKEN, refreshToken);
+            searchParams.delete('accessToken');
+            searchParams.delete('refreshToken');
+            setSearchParams(searchParams);
+
+            navigate('/');
+            dispatch(setShowToastMessage({
+                show: true,
+                message: 'login_page.response_message.login_success',
+                type: 'success'
+            }));
+        } else if (errorCode) {
+            dispatch(setShowToastMessage({
+                show: true,
+                message: 'login_page.response_message.login_failed',
+                type: 'error'
+            }));
+        }
+    }, [ accessToken, refreshToken ]);
+
 
     return (
         <div className={'flex items-center justify-center w-full h-screen'}>
